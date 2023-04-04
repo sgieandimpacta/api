cceafb842f59a6910256fd88a086b9d275d6fcb6
sgieandimpacta <128334003+sgieandimpacta@users.noreply.github.com>
2023-04-04 01:52:40 -0300

    feat: cadastrar empresas (#3)
    * feat: alterado o driver de conexão de banco para utilizar o mysql
    
    * fix: datas com valores divergentes
    
    * feat: ordenação de pagamentos por data de pagamento
    
    * fix: data de pagamento por data de vencimento
    
    * feat: cadastro de empresas

---
+++ app/Controllers/CompaniesController.ts
@@ -0,0 +1,29 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Company from 'App/Models/Company'
+
+export default class CompaniesController {
+  public async index(ctx: HttpContextContract) {
+    const companies = await Company.all()
+    return ctx.response.send(companies, true)
+  }
+
+  public async store(ctx: HttpContextContract) {
+    const company = await Company.create(ctx.request.body())
+    return ctx.response.send(company, true)
+  }
+
+  public async show(ctx: HttpContextContract) {
+    const company = await Company.find(ctx.request.param('id'))
+    return ctx.response.send(company, true)
+  }
+
+  public async update(ctx: HttpContextContract) {
+    await Company.query().where('id', ctx.request.param('id')).update(ctx.request.body())
+    return ctx.response.send(await Company.findOrFail(ctx.request.param('id')), true)
+  }
+
+  public async destroy(ctx: HttpContextContract) {
+    const company = await Company.findOrFail(ctx.request.param('id'))
+    await company.delete()
+  }
+}
