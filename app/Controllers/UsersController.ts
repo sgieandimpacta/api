d77402eab78dd0e7a9a07172a091f46cdeeb05e3
sgieandimpacta <128334003+sgieandimpacta@users.noreply.github.com>
2023-04-13 00:34:01 -0300

    fix: mudança no driver para o bcrypt (#8)

--- app/Controllers/UsersController.ts
+++ app/Controllers/UsersController.ts
@@ -2,8 +2,23 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'App/Models/User'
 
 export default class UsersController {
+  public async index(ctx: HttpContextContract) {
+    const companies = await User.all()
+    return ctx.response.send(companies, true)
+  }
+
   public async store(ctx: HttpContextContract) {
     const payment = await User.create(ctx.request.body())
     return ctx.response.send(payment, true)
+  }
+
+  public async show(ctx: HttpContextContract) {
+    const user = await User.find(ctx.request.param('id'))
+    return ctx.response.send(user, true)
+  }
+
+  public async destroy(ctx: HttpContextContract) {
+    const user = await User.findOrFail(ctx.request.param('id'))
+    await user.delete()
   }
 }
